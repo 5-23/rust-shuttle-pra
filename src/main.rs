0cde2c151f52fc5e2f30e9e7b6069eef520e69a8
5-23 dev <yhanbyeol6@gmail.com>
2023-07-29 03:11:40 +0900

    feat: add signup + login

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, fs};
 
-use axum::{routing::get, Router, response::Html, Json};
+use axum::{routing::get, Router, response::Html, Json, extract::Query};
 use tower_http::services::ServeDir;
+use serde_json::json;
+
+#[derive(serde::Deserialize, Debug)]
+struct User {
+    id: String,
+    pw: String,
+    email: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct UserLogin {
+    id: String,
+    pw: String
+}
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Copy)]
 struct Counter {
     val: isize
 }
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Copy)]
+struct Status {
+    error: bool,
+    code: usize
+}
+
+
 static mut COUNTER: Counter = Counter{val: 0};
 
 
@@ -15,6 +37,55 @@ async fn hello_world() -> Html<&'static str> {
 }
 
 
+async fn signup(q: Query<User>) -> Json<Status>{
+    let mut data: Vec<User> = serde_json::from_str(&fs::read_to_string("user.json").unwrap()).unwrap();
+    if q.id.len() > 20 || q.pw.len() > 20 || q.email.len() > 20  {
+        return Json(Status { error: true, code: 1 });
+    } 
+    for u in data.iter(){
+        if u.id == q.id{
+            return Json(Status { error: true, code: 2 });
+        }
+    }
+    data.push(User {
+        id: q.id.clone(),
+        pw: q.pw.clone(),
+        email: q.email.clone()
+    });
+
+    let mut res = String::new();
+
+    res.push_str("[");
+    for (idx, q) in data.iter().enumerate(){
+        if idx == data.len()-1{
+            res.push_str(&format!("{{\"id\": {:?}, \"pw\": {:?}, \"email\": {:?}}}", q.id, q.pw, q.email));
+        }else{
+            res.push_str(&format!("{{\"id\": {:?}, \"pw\": {:?}, \"email\": {:?}}},", q.id, q.pw, q.email));
+        }
+    }
+    res.push_str("]");
+
+    println!("{}", res);
+    fs::write("user.json", res).unwrap();
+    return Json(Status { error: false, code: 0 });
+
+}
+
+
+async fn login(q: Query<UserLogin>) -> Json<Status>{
+    let mut data: Vec<User> = serde_json::from_str(&fs::read_to_string("user.json").unwrap()).unwrap();
+
+    for u in data.iter(){
+        if q.id == u.id{
+            if q.pw != u.pw {
+                return Json(Status{ error: true, code: 1 })
+            }else{
+                break;
+            }
+        }
+    }
+    Json(Status { error: false, code: 0 })
+}
 
 
 async fn counting() -> Json<Counter> {
@@ -30,6 +101,8 @@ async fn axum(
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/cnt", get(counting))
+        .route("/signup", get(signup))
+        .route("/login", get(login))
         .nest_service("/static", ServeDir::new(sf))
         ;
 
