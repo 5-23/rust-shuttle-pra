5ec7845f0ce6f2517bb9ff1fa44071305abb35ab
5-23 dev <yhanbyeol6@gmail.com>
2023-07-28 22:45:41 +0900

    feat: add static folder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 use axum::{routing::get, Router, response::Html, Json};
+use tower_http::services::ServeDir;
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Copy)]
 struct Counter {
@@ -12,16 +15,23 @@ async fn hello_world() -> Html<&'static str> {
 }
 
 
+
+
 async fn counting() -> Json<Counter> {
     unsafe{ COUNTER.val += 1 }
     Json(unsafe { COUNTER })
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_static_folder::StaticFolder(folder = "assets")] sf: PathBuf
+) -> shuttle_axum::ShuttleAxum {
+    println!("{sf:?}");
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/cnt", get(counting));
+        .route("/cnt", get(counting))
+        .nest_service("/static", ServeDir::new(sf))
+        ;
 
     Ok(router.into())
 }
