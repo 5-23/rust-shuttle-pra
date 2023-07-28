3619955929b5a8604c7256ef37d6f74cfe8c1a9e
5-23 dev <yhanbyeol6@gmail.com>
2023-07-28 20:46:50 +0900

    fix: change text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::Html};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world() -> Html<&'static str> {
+    Html("<h1>어썸링님 그는 신이야!</h1>")
 }
 
 #[shuttle_runtime::main]
